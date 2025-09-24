4add8cc8c524a137fc6c69d93f259c777ba1f19e
Marco Fabian <marcofabianufmg@hotmail.com>
2025-09-23 22:48:13 -0300

    add random node initial structure

---
+++ n8n-node-random/src/nodes/Random/Random.node.ts
@@ -0,0 +1,53 @@
+import {
+	IExecuteFunctions,
+	INodeExecutionData,
+	INodeType,
+	INodeTypeDescription,
+} from 'n8n-workflow';
+
+export class Random implements INodeType {
+	description: INodeTypeDescription = {
+		displayName: 'Random',
+		name: 'random',
+		icon: 'file:random.svg',
+		group: ['transform'],
+		version: 1,
+		subtitle: '={{$parameter["operation"]}}',
+		description: 'Generate random numbers',
+		defaults: {
+			name: 'Random',
+		},
+		inputs: ['main'],
+		outputs: ['main'],
+		properties: [
+			{
+				displayName: 'Operation',
+				name: 'operation',
+				type: 'options',
+				noDataExpression: true,
+				options: [
+					{
+						name: 'True Random Number Generator',
+						value: 'generateRandomNumber',
+					},
+				],
+				default: 'generateRandomNumber',
+			},
+		],
+	};
+
+	async execute(this: IExecuteFunctions): Promise<INodeExecutionData[][]> {
+		const items = this.getInputData();
+		const returnData: INodeExecutionData[] = [];
+
+		for (let i = 0; i < items.length; i++) {
+			returnData.push({
+				json: {
+					randomNumber: 42,
+				},
+			});
+		}
+
+		return [returnData];
+	}
+}
